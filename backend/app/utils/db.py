bab3553a880fa2a6f9b169f1f963e53e2a8ccd27
Diego Jared <djjaredev@gmail.com>
2025-04-13 11:55:21 -0600

    Move fastapi app to backend directory

---
+++ backend/app/utils/db.py
@@ -0,0 +1,8 @@
+from sqlmodel import SQLModel, create_engine
+from app.utils.config import settings
+
+engine = create_engine(settings.DATABASE_URL, echo=True)  # echo=True for debugging
+
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
