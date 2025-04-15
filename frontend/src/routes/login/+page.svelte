deb7f14fdd963d364f9f4dd6e796320983fc2080
Diego Jared <djjaredev@gmail.com>
2025-04-14 21:43:16 -0600

    Create login page

---
+++ frontend/src/routes/login/+page.svelte
@@ -0,0 +1,17 @@
+<button class="btn signup-btn">Sign Up</button>
+<form class="login-form">
+	<h2>Login to Neonote</h2>
+	<br />
+	<div class="form-group">
+		<input type="text" id="username" name="username" placeholder=" " required />
+		<label for="username">Username</label>
+	</div>
+	<div class="form-group">
+		<input type="password" id="password" name="password" placeholder=" " required />
+		<label for="password">Password</label>
+	</div>
+	<button class="btn main-btn" type="submit">Login</button>
+</form>
+
+<style>
+</style>
