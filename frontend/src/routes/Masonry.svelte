1c677498c59dcada34a90eccd269aefdf135ee19
Diego Jared <djjaredev@gmail.com>
2025-04-30 18:28:59 -0600

    Update layout
    Rename layout -> masonry
    Update masonry()
    Update css

--- frontend/src/routes/Masonry.svelte
+++ frontend/src/routes/Masonry.svelte
@@ -9,18 +9,22 @@
 
 		const width = 240;
 
-		let columns = 5;
+		let columns = 7;
 
 		const heights = Array(columns).fill(0);
 
-		while (columns * width > window.innerWidth) {
+		const gap = 10;
+
+		while (columns * width + gap * columns + 60 >= window.innerWidth) {
+			console.log("Columns: " + columns);
 			columns--;
 		}
 
-		const gap = 10;
 		items.forEach((item, index) => {
 			let column = index % columns;
-			item.style.transform = `translate(${column * (width + gap)}px, ${heights[column]}px)`;
+			// item.style.transform = `translate(${column * (width + gap)}px, ${heights[column]}px)`;
+			item.style.top = heights[column] + "px";
+			item.style.left = column * (width + gap) + "px";
 			item.style.height = item.offsetHeight + "px"; // It is used to set the height in the CSS (by default it is not set when it is auto) so that the transition can be made correctly
 			heights[column] += item.offsetHeight + gap;
 		});
@@ -31,28 +35,38 @@
 	});
 </script>
 
-<svelte:window on:resize={masonry} />
+<svelte:window onresize={masonry} />
 
+<!-- <div class="header"></div> -->
 <div class="layout">
 	{@render notes(masonry)}
 	<div class="overlay"></div>
 </div>
 
 <style>
+	.header {
+		width: 100%;
+		height: 100px;
+		background: orangered;
+	}
+
 	.layout {
 		position: relative;
-		width: 100%;
+		flex: 1;
+		width: 99%;
 		height: 100vh;
-		background: #11111b;
+		background: inherit;
 		border: none;
 		padding: 0;
 		margin: 0;
+		overflow-y: visible;
+		align-self: center;
 	}
 
 	.overlay {
 		position: fixed;
 		width: 100%;
-		height: 100%;
+		height: 100vh;
 		background-color: rgba(0, 0, 0, 0.5);
 		opacity: 0;
 		pointer-events: none;
