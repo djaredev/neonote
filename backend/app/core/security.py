be47102794755a1e63ba08ed02ccccb7ed0275a7
Diego Jared <djjaredev@gmail.com>
2025-05-05 21:40:58 -0600

    Add security module
    Contains functions to handle auth

---
+++ backend/app/core/security.py
@@ -0,0 +1,32 @@
+from datetime import datetime, timedelta, timezone
+from typing import Any
+from passlib.context import CryptContext
+import jwt
+from app.core.config import settings
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+def create_access_token(subject: str | Any) -> str:
+    encode = {
+        "sub": str(subject),
+        "exp": datetime.now(tz=timezone.utc)
+        + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES),
+    }
+    return jwt.encode(encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
+
+
+def decode_token(token: str) -> Any:
+    print("Decode...")
+    print(settings)
+    payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
+    print(payload)
+    return payload
+
+
+def verify_password(plan_password: str, hashed_password: str) -> bool:
+    return pwd_context.verify(plan_password, hashed_password)
+
+
+def get_password_hash(password: str) -> str:
+    return pwd_context.hash(password)
