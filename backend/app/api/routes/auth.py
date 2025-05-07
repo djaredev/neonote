cc07a27b9135f278192b4a3069019d01f5ed7a69
Diego Jared <djjaredev@gmail.com>
2025-05-06 22:24:24 -0600

    Create auth routes

---
+++ backend/app/api/routes/auth.py
@@ -0,0 +1,36 @@
+from typing import Annotated
+from fastapi import APIRouter, Depends, HTTPException, Response, status
+from fastapi.security import OAuth2PasswordRequestForm
+from app.api.deps import SessionDep, UserCookie
+from app.core.security import create_access_token
+from app import service
+from app.models.user import UserPublic
+
+router = APIRouter(tags=["auth"])
+
+
+@router.post("/login", response_model=UserPublic)
+async def login(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+    session: SessionDep,
+    response: Response,
+):
+    user = service.autentication(form_data.username, form_data.password, session)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Incorrect username or password",
+        )
+    access_token = create_access_token(user.id)
+    response.set_cookie(
+        key="neonote_token",
+        value=access_token,
+        httponly=True,
+        secure=False,
+    )
+    return user
+
+
+@router.get("/whoami", response_model=UserPublic)
+async def whoami(user: UserCookie):
+    return user
