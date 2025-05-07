38f6b0ef354e4c9db15d7bdb1ac2df9804a26a20
Diego Jared <djjaredev@gmail.com>
2025-05-06 22:35:03 -0600

    Add cors to the frontend app

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,18 @@
 from fastapi import FastAPI
 from app.api.router import api_router
 from app.core.db import create_db_and_tables
+from fastapi.middleware.cors import CORSMiddleware
 
 create_db_and_tables()
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(api_router)
