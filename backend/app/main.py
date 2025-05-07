cb23e789c48a2da3bf36c2b79856fb38d026027a
Diego Jared <djjaredev@gmail.com>
2025-05-06 22:33:05 -0600

    Update main.py
    Add api router
    Initialize database

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
+from app.api.router import api_router
+from app.core.db import create_db_and_tables
 
+create_db_and_tables()
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+app.include_router(api_router)
