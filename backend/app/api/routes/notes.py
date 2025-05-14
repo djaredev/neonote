38f280986814969d61b0ee2387571a79f2058d46
Diego Jared <djjaredev@gmail.com>
2025-05-13 21:06:20 -0600

    Add endpoint to create a note

--- backend/app/api/routes/notes.py
+++ backend/app/api/routes/notes.py
@@ -1,3 +1,13 @@
 from fastapi import APIRouter
 
+from app.api.deps import CurrentUser
+from app.models import NoteCreate
+from app.service.note_service import NoteService
+
 router = APIRouter(prefix="/notes", tags=["notes"])
+
+
+@router.post("")
+async def create_note(user: CurrentUser, note: NoteCreate, service: NoteService):
+    new_note = service.create_note(user, note)
+    return new_note
