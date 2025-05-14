9a15dd6d2488d2e78ac2d1a74ca0741c8f70928c
Diego Jared <djjaredev@gmail.com>
2025-05-13 21:05:27 -0600

    Create the note route

--- backend/app/api/router.py
+++ backend/app/api/router.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
 
-from app.api.routes import users, auth
+from app.api.routes import users, auth, notes
 
 api_router = APIRouter()
 
 api_router.include_router(auth.router)
 api_router.include_router(users.router)
+api_router.include_router(notes.router)
