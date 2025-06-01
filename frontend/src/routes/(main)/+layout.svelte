b7c281715fabcde3e7ef180576f43f4a7aa5acbe
Diego Jared <djjaredev@gmail.com>
2025-05-31 22:09:48 -0600

    Create main page

---
+++ frontend/src/routes/(main)/+layout.svelte
@@ -0,0 +1,33 @@
+<script>
+	import Sidebar from "$lib/ui/Sidebar.svelte";
+	import Header from "$lib/ui/Header.svelte";
+
+	let { children } = $props();
+</script>
+
+<Header />
+
+<div class="container">
+	<Sidebar />
+	<div class="content">
+		{@render children()}
+	</div>
+</div>
+
+<style>
+	.container {
+		display: flex;
+		flex: 1;
+		overflow-y: auto;
+		/* overflow: hidden; */
+	}
+
+	.content {
+		display: flex;
+		flex-direction: column;
+		flex: 1;
+		overflow-y: auto;
+		padding-left: 60px;
+		gap: 10px;
+	}
+</style>
---
+++ frontend/src/routes/(main)/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import type { PageProps } from "./$types";
+	import MakeNote from "$lib/ui/MakeNote.svelte";
+	let { data }: PageProps = $props();
+</script>
+
+<MakeNote />
+
+<style>
+</style>
