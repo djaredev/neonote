aebf94bbec14946f3205a390b215c0aa26bf2ba1
Diego Jared <djjaredev@gmail.com>
2025-05-31 21:43:45 -0600

    Change the note state management from an object-based approach to a class + context-based approach

--- frontend/src/lib/state/note.svelte.ts
+++ frontend/src/lib/state/note.svelte.ts
@@ -1,4 +1,49 @@
-import { type NotePublic } from "@neonote/sdk";
-export const notes = $state({
-	notes: new Array<NotePublic>()
-});
+import { getContext, setContext } from "svelte";
+import { updateNote, type NotePublic } from "@neonote/sdk";
+
+class NoteState {
+	// private notes: NotePublic[] | null = $state(null);
+	private notes: NotePublic[] = $state([]);
+	constructor(notes: NotePublic[]) {
+		this.notes = notes;
+	}
+
+	create = () => {};
+
+	findById = (id: string): NotePublic | undefined => {
+		return this.notes?.find((n) => n.id === id);
+	};
+
+	getAll = () => {
+		return this.notes;
+	};
+
+	update = async (noteSnapshot: NotePublic) => {
+		const note = this.findById(noteSnapshot.id);
+		if (note && (note.title !== noteSnapshot.title || note.content !== noteSnapshot.content)) {
+			const res = await updateNote({
+				body: {
+					title: note.title,
+					content: note.content
+				},
+				path: {
+					id: note.id
+				}
+			});
+			return res;
+		}
+	};
+
+	remove = () => {};
+}
+
+const KEY = "id_noteState";
+
+export const setNoteState = (notePublic: NotePublic[]): NoteState => {
+	setContext(KEY, new NoteState(notePublic));
+	return getNoteState();
+};
+
+export const getNoteState = (): NoteState => {
+	return getContext(KEY) as NoteState;
+};
