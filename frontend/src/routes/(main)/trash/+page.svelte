aa7c0b2deb366bb1caea6ed016844cfc94f28f1c
Diego Jared <djjaredev@gmail.com>
2025-06-02 13:13:31 -0600

    (frontend) Create trash page

---
+++ frontend/src/routes/(main)/trash/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import type { PageProps } from "./$types";
+	import { setNoteState } from "$lib/state/note.svelte";
+	import Layout from "$lib/ui/Masonry.svelte";
+	import TrashedNote from "$lib/ui/note/TrashedNote.svelte";
+	let { data }: PageProps = $props();
+	let layout;
+	const noteState = setNoteState(data.data);
+	const notes = $derived(noteState.getAll());
+</script>
+
+<Layout bind:this={layout}>
+	{#each notes as note (note.id)}
+		<TrashedNote masonry={layout.masonry} id={note.id} />
+	{/each}
+</Layout>
---
+++ frontend/src/routes/(main)/trash/+page.ts
@@ -0,0 +1,12 @@
+import type { PageLoad } from "./$types";
+import { auth } from "$lib/utils/auth.svelte";
+import { getTrashedNotes, type NotePublic } from "@neonote/sdk";
+
+export const load: PageLoad = async () => {
+	await auth();
+	const data = await getTrashedNotes();
+	if (data.data) {
+		return { data: data.data.notes };
+	}
+	return { data: new Array<NotePublic>() };
+};
