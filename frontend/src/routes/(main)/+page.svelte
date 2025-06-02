67b7f39e11f6ac9845e5b96cec13b6a44e624a9e
Diego Jared <djjaredev@gmail.com>
2025-06-02 13:12:07 -0600

    (frontend) Update main page: Add Note component in Layout component

--- frontend/src/routes/(main)/+page.svelte
+++ frontend/src/routes/(main)/+page.svelte
@@ -3,9 +3,17 @@
 	import { setNoteState } from "$lib/state/note.svelte";
 	import MakeNote from "$lib/ui/MakeNote.svelte";
 	import Layout from "$lib/ui/Masonry.svelte";
+	import Note from "$lib/ui/note/Note.svelte";
+
 	let { data }: PageProps = $props();
-	setNoteState(data.data);
+	let layout;
+	const noteState = setNoteState(data.data);
+	const notes = $derived(noteState.getAll());
 </script>
 
 <MakeNote />
-<Layout />
+<Layout bind:this={layout}>
+	{#each notes as note (note.id)}
+		<Note masonry={layout.masonry} id={note.id} />
+	{/each}
+</Layout>
