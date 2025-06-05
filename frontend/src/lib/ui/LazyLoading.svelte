bdfd649c11910f75d2a886d3181ad087f24347fe
Diego Jared <djjaredev@gmail.com>
2025-06-05 14:08:42 -0600

    (frontend) Create LazyLoading component

---
+++ frontend/src/lib/ui/LazyLoading.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import { onMount } from "svelte";
+	let { loadData } = $props();
+	let lazyLoading: HTMLElement;
+
+	const observer = new IntersectionObserver(
+		(entries) => {
+			if (entries[0].isIntersecting) {
+				loadData();
+			}
+		},
+		{
+			rootMargin: "0px",
+			threshold: 1
+		}
+	);
+
+	onMount(() => {
+		observer.observe(lazyLoading);
+	});
+</script>
+
+<div class="lazy-loading" bind:this={lazyLoading}></div>
+
+<style>
+	.lazy-loading {
+		position: absolute;
+		width: 100px;
+		height: 200px;
+		visibility: hidden;
+	}
+</style>
