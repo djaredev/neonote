dd34390b2ba01eba39a9f0115f172f94c7833485
Diego Jared <djjaredev@gmail.com>
2025-06-10 11:59:48 -0600

    (frontend) Create account settings page

---
+++ frontend/src/routes/(main)/settings/account/+page.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+	import Button from "$lib/components/Button.svelte";
+	import Field from "$lib/components/Field.svelte";
+</script>
+
+<div class="profile">
+	<div class="form">
+		<label for="" class="label">Username</label>
+		<Field />
+		<label for="" class="label">Email</label>
+		<Field />
+	</div>
+	<Button class="button">Save</Button>
+</div>
+
+<style>
+	.profile {
+		display: flex;
+		flex-direction: column;
+		gap: 10px;
+		border: 1px solid #313244;
+		border-radius: 8px;
+		justify-content: center;
+		align-items: center;
+		padding: 20px;
+
+		.form {
+			display: flex;
+			width: 100%;
+			flex-direction: column;
+			gap: 10px;
+
+			.label {
+				color: #a6adc8;
+				text-align: left;
+				padding: 2px;
+			}
+		}
+
+		:global(.button) {
+			align-self: end;
+		}
+	}
+</style>
