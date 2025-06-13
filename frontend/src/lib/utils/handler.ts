058d725f661e170fe0f608ac699cdf4d4f70ef2a
Diego Jared <djjaredev@gmail.com>
2025-06-13 15:24:17 -0600

    (frontend) Create handler class to manage callback functions passed to events

---
+++ frontend/src/lib/utils/handler.ts
@@ -0,0 +1,29 @@
+class Handler<T extends any[]> {
+	private callback: ((...arg: T) => void) | null;
+	private restoreCallback: (...arg: T) => void;
+	constructor(callback: (...arg: T) => void) {
+		this.callback = callback;
+		this.restoreCallback = callback;
+	}
+
+	once = (...arg: T) => {
+		this.callback?.(...arg);
+		this.callback = null;
+	};
+
+	run = (...arg: T) => {
+		this.callback?.(...arg);
+	};
+
+	restore = () => {
+		this.callback = this.restoreCallback;
+	};
+
+	clear = () => {
+		this.callback = null;
+	};
+}
+
+export default function handler<T extends any[]>(callback: (...arg: T) => void, ref: object) {
+	return new Handler(callback.bind(ref));
+}
