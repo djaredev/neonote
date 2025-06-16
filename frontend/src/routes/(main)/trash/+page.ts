611edcd58f94fbf0f97954b787cdeac884639a4e
Diego Jared <djjaredev@gmail.com>
2025-06-15 18:24:02 -0600

    (frontend) Refactor code in +page.ts

--- frontend/src/routes/(main)/trash/+page.ts
+++ frontend/src/routes/(main)/trash/+page.ts
@@ -1,12 +1,10 @@
 import type { PageLoad } from "./$types";
 import { auth } from "$lib/utils/auth.svelte";
 import { getTrashedNotes, type NotePublic } from "@neonote/sdk";
+import { request } from "$lib/utils/request";
 
 export const load: PageLoad = async () => {
 	await auth();
 	const data = await getTrashedNotes();
-	if (data.data) {
-		return { data: data.data.notes };
-	}
-	return { data: new Array<NotePublic>() };
+	return { data: data.data ? data.data.notes : new Array<NotePublic>() };
 };
