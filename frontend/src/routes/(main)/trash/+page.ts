ccb15b86a1c960d02170dec841e68d2e274a1c72
Diego Jared <djjaredev@gmail.com>
2025-06-15 18:35:52 -0600

    (frontend) Fix imports

--- frontend/src/routes/(main)/trash/+page.ts
+++ frontend/src/routes/(main)/trash/+page.ts
@@ -1,7 +1,6 @@
 import type { PageLoad } from "./$types";
 import { auth } from "$lib/utils/auth.svelte";
 import { getTrashedNotes, type NotePublic } from "@neonote/sdk";
-import { request } from "$lib/utils/request";
 
 export const load: PageLoad = async () => {
 	await auth();
