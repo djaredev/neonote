a5482918f90bb8ff89d54589b7cadd47e315d3b0
Diego Jared <djjaredev@gmail.com>
2025-06-19 12:03:52 -0600

    (backend) Create Auth service
    Add login operation

---
+++ backend/app/service/auth_service.py
@@ -0,0 +1,18 @@
+from typing import Annotated
+
+from fastapi import Depends
+from app.core.security import verify_password
+from app.repository.user_repository import UserRepository
+
+
+class _AuthService:
+    def login(self, user_repo: UserRepository, username: str, password: str):
+        db_user = user_repo.get_by_username(username)
+        if not db_user:
+            return None
+        if not verify_password(password, db_user.hashed_password):
+            return None
+        return db_user
+
+
+AuthService = Annotated[_AuthService, Depends()]
