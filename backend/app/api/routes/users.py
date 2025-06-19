0581215bab83ee52314cadcda0d0a09e6f458108
Diego Jared <djjaredev@gmail.com>
2025-06-19 13:04:18 -0600

    (backend) Update user endpoints
    Disable create user
    
    Add endpoints:
    
    * update_password_me
    * update_user_me

--- backend/app/api/routes/users.py
+++ backend/app/api/routes/users.py
@@ -1,12 +1,23 @@
 from fastapi import APIRouter, status
-from app.models import UserCreate, UserBase
+from app.models.user import UpdatePassword, UserPublic, UserUpdate
 from app.service import UserService
 
 
 router = APIRouter(prefix="/users", tags=["users"])
 
 
-@router.post("", status_code=status.HTTP_201_CREATED, response_model=UserBase)
-async def create_user(user: UserCreate, service: UserService):
-    created_user = service.create_user(user)
-    return created_user
+# @router.post("", status_code=status.HTTP_201_CREATED, response_model=UserBase)
+# async def create_user(service: UserService, user: UserCreate):
+#     created_user = service.create_user(user)
+#     return created_user
+
+
+@router.patch("/me/password", status_code=status.HTTP_204_NO_CONTENT)
+async def update_password_me(service: UserService, passwords: UpdatePassword):
+    service.update_password(passwords)
+
+
+@router.patch("/me", status_code=status.HTTP_200_OK, response_model=UserPublic)
+async def update_user_me(service: UserService, user_update: UserUpdate):
+    updated_user = service.update_user(user_update)
+    return updated_user
