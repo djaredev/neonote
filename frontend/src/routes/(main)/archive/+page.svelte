3206726ae8d74925ac21952e70b7b7cafacf1fdb
Diego Jared <djjaredev@gmail.com>
2025-06-25 15:02:14 -0600

    (frontend) Fix layout usage

--- frontend/src/routes/(main)/archive/+page.svelte
+++ frontend/src/routes/(main)/archive/+page.svelte
@@ -3,14 +3,9 @@
 	import { setNoteState } from "$lib/state/note.svelte";
 	import Layout from "$lib/ui/Masonry.svelte";
 	import ArchivedNote from "$lib/ui/note/ArchivedNote.svelte";
+	import { getArchivedNotes } from "@neonote/sdk";
 	let { data }: PageProps = $props();
-	let layout;
-	const noteState = setNoteState(data.data);
-	const notes = $derived(noteState.getAll());
+	setNoteState(data.data);
 </script>
 
-<Layout bind:this={layout}>
-	{#each notes as note (note.id)}
-		<ArchivedNote masonry={layout.masonry} id={note.id} />
-	{/each}
-</Layout>
+<Layout getData={getArchivedNotes} NoteType={ArchivedNote} />
--- frontend/src/routes/(main)/trash/+page.svelte
+++ frontend/src/routes/(main)/trash/+page.svelte
@@ -3,14 +3,9 @@
 	import { setNoteState } from "$lib/state/note.svelte";
 	import Layout from "$lib/ui/Masonry.svelte";
 	import TrashedNote from "$lib/ui/note/TrashedNote.svelte";
+	import { getTrashedNotes } from "@neonote/sdk";
 	let { data }: PageProps = $props();
-	let layout;
-	const noteState = setNoteState(data.data);
-	const notes = $derived(noteState.getAll());
+	setNoteState(data.data);
 </script>
 
-<Layout bind:this={layout}>
-	{#each notes as note (note.id)}
-		<TrashedNote masonry={layout.masonry} id={note.id} />
-	{/each}
-</Layout>
+<Layout getData={getTrashedNotes} NoteType={TrashedNote} />
