d925ef9fad20497e94518f7b73a46d2e44c9ef48
Diego Jared <djjaredev@gmail.com>
2025-07-04 14:39:51 -0600

    (frontend) Add middleware in the client

--- frontend/src/hooks.client.ts
+++ frontend/src/hooks.client.ts
@@ -1,8 +1,9 @@
 import type { ClientInit } from "@sveltejs/kit";
 import { client } from "@neonote/sdk";
 import { ensureAuthenticated } from "$lib/utils/auth.svelte";
+import { handleError } from "$lib/utils/middlewares";
 
 export const init: ClientInit = async () => {
-	console.log("init");
 	client.interceptors.response.use(ensureAuthenticated);
+	client.interceptors.error.use(handleError);
 };
