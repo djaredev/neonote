1cb5fdbb972889bc61acf2dd764ad85b0a0a9ec6
Diego Jared <djjaredev@gmail.com>
2025-07-04 19:23:11 -0600

    (frontend) Add logic to update password

--- frontend/src/routes/(main)/settings/security/+page.svelte
+++ frontend/src/routes/(main)/settings/security/+page.svelte
@@ -1,18 +1,45 @@
 <script lang="ts">
 	import Button from "$lib/components/Button.svelte";
 	import Field from "$lib/components/Field.svelte";
+	import { updatePasswordMe, type UpdatePassword } from "@neonote/sdk";
+	import { notify } from "$lib/state/notify.svelte";
+
+	const passwords: UpdatePassword = $state({
+		current_password: "",
+		new_password: ""
+	});
+
+	let confirmPassword = $state("");
+
+	const onsubmit = async () => {
+		if (passwords.new_password !== confirmPassword) {
+			notify.error("New password and confirm new password do not match");
+			return;
+		}
+		const res = await updatePasswordMe({
+			body: passwords
+		});
+
+		if (res.response.status === 204) {
+			notify.success("Password updated");
+		}
+	};
 </script>
 
-<div class="security">
+<form {onsubmit} class="security">
 	<div class="title">Change password</div>
-	<label for="" class="label">Current password</label>
-	<Field />
-	<label for="" class="label">New password</label>
-	<Field />
-	<label for="" class="label">Confirm new password</label>
-	<Field />
-	<Button class="button">Update password</Button>
-</div>
+	<label for="currentPassword" class="label">Current password</label>
+	<Field id="currentPassword" type="password" bind:value={passwords.current_password} />
+	<label for="newPassword" class="label">New password</label>
+	<Field id="newPassword" type="password" bind:value={passwords.new_password} />
+	<label for="conNewPassword" class="label">Confirm new password</label>
+	<Field id="conNewPassword" type="password" bind:value={confirmPassword} />
+	{#if passwords.current_password && passwords.new_password && confirmPassword}
+		<Button>Update password</Button>
+	{:else}
+		<Button disabled>Update password</Button>
+	{/if}
+</form>
 
 <style>
 	.security {
