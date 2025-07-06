9e76f76eecf47d8c0956a2eac903e5174c7bcfa2
Diego Jared <djjaredev@gmail.com>
2025-07-05 20:01:08 -0600

    (frontend) Update middleware usage

--- frontend/src/hooks.client.ts
+++ frontend/src/hooks.client.ts
@@ -1,9 +1,9 @@
 import type { ClientInit } from "@sveltejs/kit";
 import { client } from "@neonote/sdk";
 import { ensureAuthenticated } from "$lib/utils/auth.svelte";
-import { handleError } from "$lib/utils/middlewares";
+import { handleHttpError } from "$lib/utils/middlewares";
 
 export const init: ClientInit = async () => {
 	client.interceptors.response.use(ensureAuthenticated);
-	client.interceptors.error.use(handleError);
+	client.interceptors.error.use(handleHttpError);
 };
