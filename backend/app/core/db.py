e7be8f20072026a47636d317a2261b4f95029938
Diego Jared <djjaredev@gmail.com>
2025-07-14 12:12:40 -0600

    (backend) Add util to initialize database and check if a user is created

--- backend/app/core/db.py
+++ backend/app/core/db.py
@@ -1,14 +1,16 @@
-from sqlmodel import SQLModel, create_engine
 from sqlmodel import SQLModel, Session, create_engine, select
+from app.core.logger import logger
 from app.core.config import settings
 from app.models import User
 from app.core.security import get_password_hash
 
-engine = create_engine(settings.DATABASE_URL, echo=True)  # echo=True for debugging
+engine = create_engine(settings.DATABASE_URL)  # echo=True for debugging
 
 
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
+
+
 def create_superuser(session: Session):
     session.add(
         User(
@@ -19,3 +21,12 @@ def create_superuser(session: Session):
     )
     session.commit()
 
+
+def init_db():
+    create_db_and_tables()
+    with Session(engine) as session:
+        user = session.exec(select(User).limit(1)).first()
+        if not user:
+            logger.info("Creating superuser...")
+            create_superuser(session)
+            logger.info("Superuser created.")
