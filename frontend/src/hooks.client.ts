d9f07e14314bf1ff845ee261cef64e3874148e90
Diego Jared <djjaredev@gmail.com>
2025-07-13 22:48:19 -0600

    (frontend) Add default error page

--- frontend/src/hooks.client.ts
+++ frontend/src/hooks.client.ts
@@ -1,4 +1,4 @@
-import type { ClientInit } from "@sveltejs/kit";
+import type { ClientInit, HandleClientError } from "@sveltejs/kit";
 import { client } from "@neonote/sdk";
 import { ensureAuthenticated } from "$lib/utils/auth.svelte";
 import { handleHttpError } from "$lib/utils/middlewares";
@@ -6,4 +6,10 @@ import { handleHttpError } from "$lib/utils/middlewares";
 export const init: ClientInit = async () => {
 	client.interceptors.response.use(ensureAuthenticated);
 	client.interceptors.error.use(handleHttpError);
+};
+
+export const handleError: HandleClientError = ({ error, event, status, message }) => {
+	return {
+		message: "Something went wrong. Please try again."
+	};
 };
---
+++ frontend/src/routes/+error.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+	import { page } from "$app/state";
+	import { CircleXIcon } from "@lucide/svelte";
+</script>
+
+<div class="container">
+	<div class="error">
+		<CircleXIcon size={50} />
+		<h1>{page.error?.message}</h1>
+	</div>
+</div>
+
+<style>
+	* {
+		box-sizing: border-box;
+	}
+	.container {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		width: 100%;
+		flex: 1;
+		color: #f38ba8;
+		padding: 20px;
+	}
+	.error {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		gap: 20px;
+		width: 600px;
+		padding: 20px;
+		border: 1px solid #313244;
+		border-radius: 10px;
+		box-shadow: 0 20px 60px rgba(0, 0, 0, 0.45);
+		overflow: hidden;
+		@media (width <= 600px) {
+			& {
+				flex-direction: column;
+				width: 100%;
+				height: auto;
+			}
+		}
+	}
+</style>
