032602ed4a4ccfae9a11d994a980a8f7ef3f0a27
Diego Jared <djjaredev@gmail.com>
2025-07-16 13:30:06 -0600

    (sdk) Update sdk:
    Update baseUrl to use local url used in produccion
    Update input to use file in directory

--- sdk/typescript-sdk/src/auth-client.ts
+++ sdk/typescript-sdk/src/auth-client.ts
@@ -1,6 +1,5 @@
 import { createClient } from "@hey-api/client-fetch";
 
 export const authclient = createClient({
-  baseUrl: "http://localhost:8000",
   credentials: "include",
 });
--- sdk/typescript-sdk/src/custom-client.ts
+++ sdk/typescript-sdk/src/custom-client.ts
@@ -2,6 +2,6 @@ import type { CreateClientConfig } from "./client/client.gen";
 
 export const createClientConfig: CreateClientConfig = (config) => ({
   ...config,
-  baseUrl: "http://localhost:8000",
   credentials: "include",
+  // throwOnError: true,
 });
