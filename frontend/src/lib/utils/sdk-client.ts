bd4a6423bbff82c99b5dfbf064cbc0d9619b10c7
Diego Jared <djjaredev@gmail.com>
2025-07-16 13:17:21 -0600

    (frontend) Handle environment variables with Vite instead of Svelte

--- frontend/src/lib/utils/sdk-client.ts
+++ frontend/src/lib/utils/sdk-client.ts
@@ -1,9 +1,8 @@
 import { client, authclient } from "@neonote/sdk";
 import { ensureAuthenticated } from "$lib/utils/auth.svelte";
 import { handleHttpError } from "$lib/utils/middlewares";
-import { PUBLIC_DEV_API_URL, PUBLIC_PROD_API_URL, PUBLIC_ENVIRONMENT } from "$env/static/public";
 
-export const API_URL = PUBLIC_ENVIRONMENT === "dev" ? PUBLIC_DEV_API_URL : PUBLIC_PROD_API_URL;
+export const API_URL = import.meta.env.VITE_API_URL;
 
 export const setUpSDKClient = () => {
 	authclient.setConfig({ baseUrl: API_URL });
