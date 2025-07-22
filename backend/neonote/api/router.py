0cd06e1c1cbf16ef752b91ee9119d20c46728b9d
Diego Jared <djjaredev@gmail.com>
2025-07-22 12:57:03 -0600

    (backend) Add health check endpoint

--- backend/neonote/api/router.py
+++ backend/neonote/api/router.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from neonote.api.routes import users, auth, notes, api_docs
+from neonote.api.routes import users, auth, notes, api_docs, utils
 from neonote.core.config import settings
 
 api_router = APIRouter()
@@ -7,6 +7,7 @@ api_router = APIRouter()
 api_router.include_router(auth.router)
 api_router.include_router(users.router)
 api_router.include_router(notes.router)
+api_router.include_router(utils.router)
 
 if settings.ENVIRONMENT == "dev":
     api_router.include_router(api_docs.router)
